b1f382861247adaf0a324240c25d30b533fb67fc
SaqibAA <ansarisaqib8286@gmail.com>
2022-06-04 10:11:52 -0700

    Flutter Local Notification

---
+++ lib/local_notification.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotification extends StatefulWidget {
+  const LocalNotification({Key? key}) : super(key: key);
+
+  @override
+  State<LocalNotification> createState() => LocalNotificationState();
+}
+
+class LocalNotificationState extends State<LocalNotification> {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  // initilize
+
+  Future initialize() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings("app_icon");
+
+    IOSInitializationSettings iosInitializationSettings =
+        const IOSInitializationSettings();
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: androidInitializationSettings,
+            iOS: iosInitializationSettings);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    // notification();
+  }
+
+  Future<void> notification() async {
+    var android = const AndroidNotificationDetails(
+      "id", "channel", channelDescription: 'your channel description',
+      importance: Importance.max,
+      priority: Priority.high,
+      icon: "mipmap/ic_launcher",
+      // ticker: 'ticker',
+    );
+
+    var ios = const IOSNotificationDetails();
+
+    var platform = NotificationDetails(android: android, iOS: ios);
+
+    await flutterLocalNotificationsPlugin.show(
+        0, "Notification Title", "Notification Description", platform,
+        payload: "Welcome to demo app");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Local Notification'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () => notification(),
+              child: const Text('Click For Local Notification')),
+        ));
+  }
+}
